d3556e087a808fdf8555a8708ad8c12cb2d3d3ba
minnu876 <minhaj209a@aptechsite.net>
2024-04-26 18:47:46 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_user_name = new System.Windows.Forms.Label();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.lbl_phone = new System.Windows.Forms.Label();
+            this.lbl_gender = new System.Windows.Forms.Label();
+            this.lbl_response = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(371, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lbl_user_name
+            // 
+            this.lbl_user_name.AutoSize = true;
+            this.lbl_user_name.Location = new System.Drawing.Point(210, 57);
+            this.lbl_user_name.Name = "lbl_user_name";
+            this.lbl_user_name.Size = new System.Drawing.Size(59, 13);
+            this.lbl_user_name.TabIndex = 1;
+            this.lbl_user_name.Text = "user_name";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(321, 299);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(155, 23);
+            this.btn_submit.TabIndex = 2;
+            this.btn_submit.Text = "submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(371, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(371, 166);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(211, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(371, 223);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(211, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(210, 115);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(52, 13);
+            this.lbl_password.TabIndex = 6;
+            this.lbl_password.Text = "password";
+            // 
+            // lbl_phone
+            // 
+            this.lbl_phone.AutoSize = true;
+            this.lbl_phone.Location = new System.Drawing.Point(210, 169);
+            this.lbl_phone.Name = "lbl_phone";
+            this.lbl_phone.Size = new System.Drawing.Size(40, 13);
+            this.lbl_phone.TabIndex = 7;
+            this.lbl_phone.Text = "phone ";
+            // 
+            // lbl_gender
+            // 
+            this.lbl_gender.AutoSize = true;
+            this.lbl_gender.Location = new System.Drawing.Point(210, 223);
+            this.lbl_gender.Name = "lbl_gender";
+            this.lbl_gender.Size = new System.Drawing.Size(40, 13);
+            this.lbl_gender.TabIndex = 8;
+            this.lbl_gender.Text = "gender";
+            this.lbl_gender.Click += new System.EventHandler(this.lbl_gender_Click);
+            // 
+            // lbl_response
+            // 
+            this.lbl_response.AutoSize = true;
+            this.lbl_response.Location = new System.Drawing.Point(215, 304);
+            this.lbl_response.Name = "lbl_response";
+            this.lbl_response.Size = new System.Drawing.Size(66, 13);
+            this.lbl_response.TabIndex = 9;
+            this.lbl_response.Text = "lbl_response";
+            this.lbl_response.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(703, 238);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbl_response);
+            this.Controls.Add(this.lbl_gender);
+            this.Controls.Add(this.lbl_phone);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.lbl_user_name);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_user_name;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Label lbl_phone;
+        private System.Windows.Forms.Label lbl_gender;
+        private System.Windows.Forms.Label lbl_response;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
